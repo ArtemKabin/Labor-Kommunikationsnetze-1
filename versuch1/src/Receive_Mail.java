0f66c6e0f3704f142a4c895ba21ce930d4486b22
Ryan <r.c.babcock@icloud.com>
2025-04-09 12:40:49 +0000

    First push

--- versuch1/src/Receive_Mail.java
+++ versuch1/src/Receive_Mail.java
@@ -3,6 +3,12 @@ import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.Session;
 import javax.mail.Store;
+import java.util.*;
+import javax.activation.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.naming.*;
+
 
 public class Receive_Mail {
 	public static void main(String[] args) throws Exception {
@@ -11,7 +17,22 @@ public class Receive_Mail {
 	
 	public static void fetchMail() {
 		try {
-			// your code here
+			Properties props = new Properties();
+			props.put("mail.store.protocol", "pop3");
+			//props.put("mail.debug", "true");
+			//props.put("mail.debug.quote", "true");
+			Session session = Session.getInstance(props, null);
+			//session.setDebug(true);
+			
+			Store store = session.getStore();
+			store.connect("localhost", "labrat", "kn1lab");
+
+			Folder folder = store.getFolder("INBOX");
+			folder.open(Folder.READ_ONLY);
+			Message[] messages = folder.getMessages();
+			for (int i = 0; i < messages.length; i++){
+				System.out.println("Nummer: " + messages[i].getMessageNumber() + "\nSubject: " + messages[i].getSubject() + "\nDatum: " + messages[i].getSentDate() + "\nSender: " + messages[i].getFrom()[0] + "\nInhalt: " + messages[i].getContent());
+			}
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- versuch1/src/Send_Mail.java
+++ versuch1/src/Send_Mail.java
@@ -1,11 +1,16 @@
-import java.util.Properties;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage; 
+import javax.mail.internet.MimeMessage;
+import javax.naming.InitialContext; 
+import java.util.*;
+import javax.activation.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.naming.*;
 
 public class Send_Mail {
 	public static void main(String[] args) {
@@ -14,7 +19,19 @@ public class Send_Mail {
 	
 	public static void sendMail() {
 		try {
-			// your code here
+			Properties props = new Properties();
+    		props.put("mail.smtp.host", "localhost");
+    		Session session = Session.getInstance(props, null);
+
+			Message msg = new MimeMessage(session);
+			msg.setSubject("First Mail");
+			msg.setSentDate(new Date());
+			msg.setFrom();
+			msg.setRecipients(Message.RecipientType.TO, 
+  				InternetAddress.parse("labrat@localhost", false));
+			msg.setText("Hello this is a Test");
+
+			Transport.send(msg);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
